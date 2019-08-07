f658ad693b3ab6e0dbf03e0925b71655f178bd82
Lilla <vajdalil.hu@gmail.com>
2019-08-07 08:11:41 +0200

    init spring

---
+++ src/main/java/com/lilway/bookflight/BookflightApplication.java
@@ -0,0 +1,13 @@
+package com.lilway.bookflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookflightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookflightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lilway/bookflight/BookflightApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lilway.bookflight;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookflightApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
